393b3b7aa592518ea73abacf3e1ac8837449351d
Sohar Gabor <soharka3@gmail.com>
2019-10-12 17:17:32 +0200

    Add variables for starting

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
 int main() {
+	int integer = 8;
+	char a = 65;
 	std::cout << "Hello Academy!" << std::endl;
+	std::cout << "value is > " << integer << " size is > " << sizeof(integer) << std::endl;
 }
