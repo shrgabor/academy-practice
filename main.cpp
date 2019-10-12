ea65585f68083f02a2ac3a8bf46514bbd688c10f
Sohar Gabor <soharka3@gmail.com>
2019-10-12 17:50:50 +0200

    Add pointers example

--- main.cpp
+++ main.cpp
@@ -5,4 +5,8 @@ int main() {
 	char a = 65;
 	std::cout << "Hello Academy!" << std::endl;
 	std::cout << "value is > " << integer << " size is > " << sizeof(integer) << std::endl;
+
+	// POINTERS
+	std::cout << &integer << std::endl;
+	int* pointer = &integer;
 }
